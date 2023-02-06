d685ea9575c6b5627a968536c70b0e389bb13bee
luanitachi <luanitachi@hotmail.com>
2023-02-05 21:52:28 -0300

    design update

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const url = "https://pokeapi.co/api/v2/pokemon/";
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
+let nome = document.querySelector(".nome");
+let tipo = document.querySelector(".tipo");
+let img = document.querySelector(".img");
 
 btn.addEventListener("click", () => {
     let pokemon = input.value;
@@ -8,9 +11,20 @@ btn.addEventListener("click", () => {
     callPokemon(pokemon)
 })
 
- function callPokemon(pokemon) {
-    let requisition = fetch(url + pokemon).then(response => response.json()).catch((error) => {console.log(error)})
-    requisition.then(data => console.log(data));
+function callPokemon(pokemon) {
+    let requisition = fetch(url + pokemon).then(response => response.json()).catch((error) => { console.log(error) })
+    requisition.then((data) => {
+        nome.innerHTML = "Nome: " + data.name;
+        let types = [];
+        data.types.map((type) => {
+            types.push(type.type.name)
+        });
+        tipo.innerHTML = `<p> Tipo: ${types.join(", ")} </p>`;
+        let urlImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+        console.log(data)
+        img.src = data.sprites.other["official-artwork"].front_default;
+
+    })
 }
 
 

c050c6e1cb1943ecf3740ab0085a3f8728716c03
luanitachi <luanitachi@hotmail.com>
2023-02-06 03:45:31 -0300

    design upgrade

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let btn = document.querySelector(".btn");
 let nome = document.querySelector(".nome");
 let tipo = document.querySelector(".tipo");
 let img = document.querySelector(".img");
+let peso = document.querySelector(".peso");
+let habili = document.querySelector(".habilidades");
 
 btn.addEventListener("click", () => {
     let pokemon = input.value;
@@ -16,13 +18,18 @@ function callPokemon(pokemon) {
     requisition.then((data) => {
         nome.innerHTML = "Nome: " + data.name;
         let types = [];
+        let abilities = [];
         data.types.map((type) => {
             types.push(type.type.name)
         });
         tipo.innerHTML = `<p> Tipo: ${types.join(", ")} </p>`;
-        let urlImg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
         console.log(data)
         img.src = data.sprites.other["official-artwork"].front_default;
+        peso.innerHTML = "Peso: " + data.weight / 10 + " kg";
+        data.abilities.map((abl) => {
+            abilities.push(abl.ability.name)
+        })
+        habili.innerHTML = `Habilidades: ${abilities.join(", ")}`
 
     })
 }
